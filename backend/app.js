a9931ae145dfbada6540b9e3bb50427dd0b6f834
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2018-08-01 16:31:12 +0200

    API COMMUNITIES ROUTE : - All routes has been added as stated in the API doc convention, there are 2 TODO in the file related to user API rouotes

--- backend/app.js
+++ backend/app.js
@@ -57,7 +57,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
+app.use('/uploads', express.static(path.join(__dirname, 'uploads')));
 
 // Call routes
 app.use('/', indexRouter);
--- backend/routes/communities.js
+++ backend/routes/communities.js
@@ -1,28 +1,62 @@
 const express = require('express');
-var stringify = require('json-stringify');
 const router = express.Router();
 const Community = require('../models/community');
 const mongoose = require('mongoose');
+const multer = require('multer');
 
+const storage = multer.diskStorage({
+    destination : function(req, file, cb) {
+        cb(null, './uploads/');
+    },
+    filename: function(req, file, cb) {
+        // Rename the uplaoded file
+        cb(null, new Date().toISOString() + file.originalname);
+    }    
+});
 
-// route '/'
+const fileFilter = (req, file, cb) => {
+    // Define the extension of the file
+    if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png')
+        cb(null, true);
+    else
+        cb(null, false);
+};
+
+const upload = multer({
+    storage: storage, 
+    limits: {
+    fileSize: 1024 * 1024 * 5
+    },
+    fileFilter : fileFilter
+});
+
+/*
+** API [GET] [POST] for route /communites
+*/
+
 router.get('/', (req, res, next) => {
-    let counting = 0;
+    let nb = 0;
 
+    Community.count().exec()
+    .then(count => {
+        nb = count;
+    });
+    //TODO nbrOfUsers
     Community.find()
+    //TODO nbrOfUsers
+    .select("communityId communityName dateOfCreation dateOfLastUpdate communityIsActive")
     .exec()
-    .then(docs => {
-
-        Community.find().count()
-        .exec()
-        .then(docs1 => {
-            res.status(200).json(docs1);
-        })
-        .catch();
+    .then(communities => {
+        if (communities.length === 0) {
+            return res.status(404).json({
+                message: "Communities not found!"
+            })
+        } else {
         res.status(200).json({
-            Number: counting,
-            Commuinities: docs
+            nbrCommunities: nb,
+            Commuinities: communities
         });
+    }
     })
     .catch(err => {
         res.status(500).json({
@@ -32,12 +66,13 @@ router.get('/', (req, res, next) => {
 });
 
 
-router.post('/', (req, res, next) => {
+router.post('/', upload.single('communityLogo'), (req, res, next) => {
+    console.log('Response: ', req.file);
     const community = new Community({
         _id : new mongoose.Types.ObjectId,
         communityId: req.body.communityId,
         communityName: req.body.communityName,
-        communityLogo: req.body.communityLogo,
+        communityLogo: req.file.path,
         communityAdmin: req.body.communityAdmin,
         communityMembers: req.body.communityMembers,
         dateOfCreation: req.body.dateOfCreation,
@@ -51,21 +86,91 @@ router.post('/', (req, res, next) => {
     .then(res => {
         console.log(res);
     })
-    .catch(err => console.log(err));
-    res.status(201).json({
-        message: 'Handling [POST] request to /communites',
-        CreateCommunity: community
+    .catch(err => {
+        res.status(500).json({
+            Error: err
+        })
     });
 });
 
-// rout /:id
-router.get('/:id', (req, res, next) => {
-    const _id = req.params.id;
-     Community.find()
+
+/*
+** API route [GET] for /communities/active
+*/
+
+router.get('/active', (req, res, next) => {
+    Community.find({
+        communityIsActive: true 
+    })
+    .select("communityId communityName dateOfCreation dateOfLastUpdate communityIsActive")
     .exec()
-    .then(res => {
-        console.log(res);
-        res.status(200).json(res);
+    .then(activeCom => {
+        if (activeCom.length === 0) {
+            return res.status(404).json({
+                message: "There are no active communities!"
+            })
+        } else {
+        res.status(200).json({
+            Communities: activeCom
+        });
+    }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    })
+});
+
+/* 
+** API route [GET] for /communities/isNotActive
+*/
+
+
+router.get('/isNotActive', (req, res, next) => {
+    Community.find({
+        communityIsActive: false 
+    })
+    .select("communityId communityName dateOfCreation dateOfLastUpdate communityIsActive")
+    .exec()
+    .then(isNOtActiveCom => {
+        if (isNOtActiveCom.length === 0) {
+            return res.status(404).json({
+                message: "There are no deactivated communities!"
+            })
+        } else {
+        res.status(200).json({
+            Communities: isNOtActiveCom
+        });
+    }
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    })
+});
+
+/*
+** API routes [GET] [DELETE] [PATCH] for /communities/id
+*/
+
+router.get('/:id', (req, res, next) => {
+    const id = req.params.id;
+     Community.find({
+        communityId: id
+    })
+    .exec()
+    .then(com => {
+        if (com.length === 0) {
+            return res.status(404).json({
+                message: "Community not found!"
+            })
+        } else {
+        res.status(200).json({
+            community: com
+        });
+    }
     })
     .catch(err => {
         console.log(err);
@@ -75,26 +180,52 @@ router.get('/:id', (req, res, next) => {
     });
 });
 
-router.post('/:id', (req, res, next) => {
+router.patch('/:id',upload.single('communityLogo'), (req, res, next) => {
     const id = req.params.id;
-    res.status(200).json({
-        message: 'Handling [POST/:id] request to /communites/' + id,
-    });
-});
-
-router.patch('/:id', (req, res, next) => {
-    const id = req.params.id;
-    res.status(200).json({
-        message: 'Handling [PATCH/:id] request to /communites/' + id,
+    
+    Community.update(
+        {communityId: id },
+        {$set: 
+            {
+                communityId: req.body.communityId,
+                communityName: req.body.communityName,
+                communityLogo: req.file.path,
+                communityAdmin: req.body.communityAdmin,
+                communityMembers: req.body.communityMembers,
+                dateOfCreation: req.body.dateOfCreation,
+                dateOfLastUpdate: req.body.dateOfLastUpdate,
+                companyName: req.body.companyName,
+                clientId: req.body.clientId,
+                communityIsActive: req.body.communityIsActive
+            }
+        })
+    .exec()
+    .then(results => {
+        req.status(200).json({
+            success: results
+        })
+    })
+    .catch(err => {
+        req.status(500).json({
+            Error: err
+        })
     });
 });
 
 router.delete('/:id', (req, res, next) => {
     const id = req.params.id;
-    res.status(200).json({
-        message: 'Handling [DELETE/:id] request to /communites/' + id,
+    Community.remove({
+        communityId: id
+    })
+    .exec()
+    .then(results => {
+        res.status(200).json(results);
+    })
+    .catch(err => {
+        res.status(500).json({
+            error: err
+        });
     });
 });
-
 
 module.exports = router;
