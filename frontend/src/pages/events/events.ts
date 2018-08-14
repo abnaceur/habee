79d498340c813d66fd513729dec21c9b5c81a77c
mathias <mathiaspeterhorst@gmail.com>
2018-08-14 17:22:59 +0200

    ADDED SECONDARY MENU ON EVENTS
    It was a test, it's not here to stay

--- frontend/src/pages/events/events.ts
+++ frontend/src/pages/events/events.ts
@@ -13,7 +13,7 @@ export class EventsPage {
 	// This will be replaced by events retrieved from the server through http requests.
 	// For now, we will use these hardcoded data
 	constructor(public navCtrl: NavController, public navParams: NavParams) {
-		this.allEvents = [new Event("../../assets/imgs/event1.jpg", "Football Match", "OM vs PSG. Allez l'OM!!!", "Jeudi 07 Septembre 2019",
+		this.allEvents = [new Event("../../assets/imgs/event1.jpg", "Football Match", "OM vs PSG. Allez l'OM!!! On amène les feux d'artifices, on va avec les ultras!!!!!", "Jeudi 07 Septembre 2019",
 			10, "stade velodrome", "2h", "Abdel"), new Event("../../assets/imgs/event2.jpg", "Yoga", "Relaxation et bien être", "Lundi 12 Octobre 2022",
 			3, "sale de yoga", "45min", "Jean")];
 		console.log("Initialized allEvents");
