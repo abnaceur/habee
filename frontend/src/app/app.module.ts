766d2e2c31543ad356cb9373885753bd2f320492
Abdeljalil NACEUR <contact@naceur-abdeljalil.com>
2018-08-15 13:13:42 +0200

    FIX CONFLICTS

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -3,38 +3,37 @@ import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { StatusBar } from '@ionic-native/status-bar';
+import { HttpModule } from '@angular/http';
 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
-import { TabsPageModule } from "../pages/tabs/tabs.module";
-import { ProfilePageModule } from "../pages/profile/profile.module";
-import { MyEventsPageModule } from "../pages/my-events/my-events.module";
-import { EventsPageModule } from "../pages/events/events.module";
-import { BargainsPageModule } from "../pages/bargains/bargains.module";
+import { LoginPage } from '../pages/login/login';
+import { LoginProvider } from '../providers/login/login';
+
+
 
 @NgModule({
-	declarations: [
-		MyApp,
-		HomePage,
-	],
-	imports: [
-		BrowserModule,
-		IonicModule.forRoot(MyApp),
-		TabsPageModule,
-		ProfilePageModule,
-		MyEventsPageModule,
-		EventsPageModule,
-		BargainsPageModule
-	],
-	bootstrap: [IonicApp],
-	entryComponents: [
-		MyApp,
-		HomePage,
-	],
-	providers: [
-		StatusBar,
-		SplashScreen,
-		{provide: ErrorHandler, useClass: IonicErrorHandler}
-	]
+  declarations: [
+    MyApp,
+    LoginPage,
+    HomePage,
+  ],
+  imports: [
+    HttpModule,
+    BrowserModule,
+    IonicModule.forRoot(MyApp)
+  ],
+  bootstrap: [IonicApp],
+  entryComponents: [
+    MyApp,
+    LoginPage,
+    HomePage
+  ],
+  providers: [
+    StatusBar,
+    SplashScreen,
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    LoginProvider
+  ]
 })
-export class AppModule {}
+export class AppModule { }
