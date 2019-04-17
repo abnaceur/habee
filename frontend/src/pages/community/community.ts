20bfb4a23d38176df7d993140191d4308894cb64
root <hisec4ever@gmail.com>
2019-04-17 22:33:38 +0200

    COMMUNITY : - Navigation logic added.

--- frontend/src/pages/community/community.ts
+++ frontend/src/pages/community/community.ts
@@ -1,6 +1,12 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
+import { UtilsProvider } from "../../providers/utils/utils";
+
+import { CommunityProvider } from "../../providers/community/community";
+
+import { environment as ENV } from "../../environments/environment";
+
 /**
  * Generated class for the CommunityPage page.
  *
@@ -15,11 +21,43 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class CommunityPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  public url = ENV.BASE_URL;
+  public tabParams;
+
+  page = 0;
+  perPage = 0;
+  totalData = 0;
+  totalPage = 0;
+
+  public myCommunitiesBorder = "5px solid darkgrey";
+  public myCommunitiesBorderDisplay = "initial";
+  public communitiesByInvteBorder = "";
+  public communitiesByInvteBorderDisplay = "none";
+
+  constructor(
+    public navCtrl: NavController, 
+    public navParams: NavParams, 
+    private communityProvider: CommunityProvider,
+    private utils: UtilsProvider,
+   ) {
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad CommunityPage');
+  }
+
+  selectMyCommunities() {
+    this.myCommunitiesBorder = "5px solid darkgrey";
+    this. myCommunitiesBorderDisplay = "initial";
+    this.communitiesByInvteBorder = "";
+    this.communitiesByInvteBorderDisplay = "none";
+  }
+
+  selectCommunitiesByInvte() {
+    this.myCommunitiesBorder = "";
+    this. myCommunitiesBorderDisplay = "none";
+    this.communitiesByInvteBorder = "5px solid darkgrey";
+    this.communitiesByInvteBorderDisplay = "initial";
   }
 
 }
